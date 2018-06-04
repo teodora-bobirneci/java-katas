ed39a664b8b719213ecd402750a1007ff6fa4760
Teodora Bobirneci <teodora.bobirneci@pearson.com>
2018-06-04 17:50:16 +0300

    Berlin Clock - first test and functionality

--- src/main/java/katas/berlinclock/BerlinClock.java
+++ src/main/java/katas/berlinclock/BerlinClock.java
@@ -1,5 +1,17 @@
 package katas.berlinclock;
 
-public class BerlinClock
+class BerlinClock
 {
+	private final String[] digitalTime;
+
+	BerlinClock(String time)
+	{
+		this.digitalTime = time.split(":");
+	}
+
+
+	String topLight()
+	{
+		return Integer.parseInt(digitalTime[2]) % 2 == 0 ? "Y" : "O";
+	}
 }
--- src/test/java/katas/berlinclock/BerlinClockTest.java
+++ src/test/java/katas/berlinclock/BerlinClockTest.java
@@ -1,6 +1,18 @@
 package katas.berlinclock;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class BerlinClockTest
 {
+
+	@Test
+	public void whenEvenSeconds_TopYellowLightIsOn()
+	{
+		BerlinClock clock = new BerlinClock("19:32:54");
+		assertEquals("Y", clock.topLight());
+	}
 
 }

3480ccc48a8e46923584236c042abb7135b079d4
Teodora Bobirneci <teobobirneci@yahoo.com>
2018-06-19 23:08:44 +0300

    josephus kata - tough one

--- src/main/java/katas/josephus/Josephus.java
+++ src/main/java/katas/josephus/Josephus.java
@@ -1,15 +1,52 @@
 package katas.josephus;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 
-public class Josephus
-{
-//test github
-	public static <T> List<T> josephusPermutation(final List<T> items, final int k)
-	{
-		return Collections.emptyList();
-	}
+class Josephus {
+
+    static <T> List<T> josephusPermutation(final List<T> items, final int k) {
+        if (items.isEmpty())
+            return Collections.emptyList();
+
+        int[] visited = new int[items.size()];
+        Arrays.fill(visited, 0);
+        List<T> result = new ArrayList<>();
+
+        int pos = (k - 1) % visited.length;
+        while (unvisitedCount(visited) > 0) {
+            result.add(items.get(pos));
+            visited[pos] = 1;
+            pos = nextKth(visited, k, pos);
+        }
+        return result;
+    }
+
+    private static int nextKth(int[] visited, int k, int pos) {
+        int count = 0;
+        for (int i = 1; i <= k * visited.length; i++) {
+            if (visited[(pos + i % visited.length) % visited.length] == 0) {
+                count++;
+                if (count == k) {
+                    return (pos + i % visited.length) % visited.length;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static int unvisitedCount(int[] visited) {
+        int sum = 0;
+
+        for (int i : visited) {
+            if (i == 0)
+                sum += 1;
+        }
+
+        return sum;
+    }
 
 }

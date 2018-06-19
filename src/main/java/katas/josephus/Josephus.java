9746498d61e6b9c56e6be1c55122a8daaa4ac490
Teodora Bobirneci <teodora.bobirneci@pearson.com>
2018-06-19 12:00:51 +0300

    Josephus permutation kata - structure

---
+++ src/main/java/katas/josephus/Josephus.java
@@ -0,0 +1,15 @@
+package katas.josephus;
+
+import java.util.Collections;
+import java.util.List;
+
+
+public class Josephus
+{
+
+	public static <T> List<T> josephusPermutation(final List<T> items, final int k)
+	{
+		return Collections.emptyList();
+	}
+
+}

74b541278e6d2bf7c29983f93513e1006152f508
Teodora Bobirneci <teobobirneci@yahoo.com>
2018-07-03 22:27:54 +0300

    battle ships kata requirement

---
+++ src/main/java/katas/battleships/BattleShips.java
@@ -0,0 +1,11 @@
+package katas.battleships;
+
+import java.util.Map;
+
+class BattleShips {
+
+    static Map<String, Double> damagedOrSunk(final int[][] board, final int[][] attacks) {
+        // Your code here...
+        return null;
+    }
+}
---
+++ src/test/java/katas/battleships/BattleShipsTest.java
@@ -0,0 +1,43 @@
+package katas.battleships;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class BattleShipsTest {
+    @Test
+    public void example1() {
+        int[][] board = new int[][]{new int[]{0, 0, 1, 0},
+                new int[]{0, 0, 1, 0},
+                new int[]{0, 0, 1, 0}};
+        int[][] attacks = new int[][]{new int[]{3, 1}, new int[]{3, 2}, new int[]{3, 3}};
+
+        Map<String, Double> expected = new HashMap<>();
+        expected.put("sunk", 1.0);
+        expected.put("damaged", .0);
+        expected.put("notTouched", .0);
+        expected.put("points", 1.0);
+
+        assertEquals(expected, BattleShips.damagedOrSunk(board, attacks));
+    }
+
+    @Test
+    public void example2() {
+        int[][] board = new int[][]{new int[]{3, 0, 1},
+                new int[]{3, 0, 1},
+                new int[]{0, 2, 1},
+                new int[]{0, 2, 0}};
+        int[][] attacks = new int[][]{new int[]{2, 1}, new int[]{2, 2}, new int[]{3, 2}, new int[]{3, 3}};
+
+        Map<String, Double> expected = new HashMap<>();
+        expected.put("sunk", 1.0);
+        expected.put("damaged", 1.0);
+        expected.put("notTouched", 1.0);
+        expected.put("points", 0.5);
+
+        assertEquals(expected, BattleShips.damagedOrSunk(board, attacks));
+    }
+}
